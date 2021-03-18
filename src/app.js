4ea1f94b85424abcc3d88556801d5fb6ceea721f
jrodrguez08 <jrodrguez08@gmail.com>
2021-03-18 01:06:25 -0600

    Adds sass

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
     res.render('index', {
