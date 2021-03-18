e29c30331dbecc71a2f80e57093e78511a0b000d
jrodrguez08 <jrodrguez08@gmail.com>
2021-03-18 11:53:05 -0600

    Pugging

--- src/app.js
+++ src/app.js
@@ -6,15 +6,17 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'templates'));
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-    res.render('index', {
-        test: 'hello pug'
-    });
+    res.render('views/index');
+});
+
+app.get('*', (req, res) => {
+    res.render('views/404');
 });
 
 app.listen(3000, () => {
     console.log('Server up')
-})
+});
